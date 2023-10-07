db68f2ca9152a138fe1858089b4b0aac8f6df89a
= <126682243+mammadovturqay@users.noreply.github.com>
2023-10-07 19:06:15 +0400

    Add project files.

---
+++ TaskWinformApp/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace TaskWinformApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxsoyad = new System.Windows.Forms.TextBox();
+            this.textboxad = new System.Windows.Forms.TextBox();
+            this.textboxataadi = new System.Windows.Forms.TextBox();
+            this.textBoXaxtaris = new System.Windows.Forms.TextBox();
+            this.buttonaxtar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textboxolke = new System.Windows.Forms.TextBox();
+            this.textboxseher = new System.Windows.Forms.TextBox();
+            this.textboxtelefon = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Anketa";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Soyad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(12, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 28);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ad";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(12, 227);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 28);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ata adi";
+            // 
+            // textBoxsoyad
+            // 
+            this.textBoxsoyad.Location = new System.Drawing.Point(85, 126);
+            this.textBoxsoyad.Name = "textBoxsoyad";
+            this.textBoxsoyad.Size = new System.Drawing.Size(355, 27);
+            this.textBoxsoyad.TabIndex = 4;
+            // 
+            // textboxad
+            // 
+            this.textboxad.Location = new System.Drawing.Point(85, 176);
+            this.textboxad.Name = "textboxad";
+            this.textboxad.Size = new System.Drawing.Size(355, 27);
+            this.textboxad.TabIndex = 5;
+            // 
+            // textboxataadi
+            // 
+            this.textboxataadi.Location = new System.Drawing.Point(85, 231);
+            this.textboxataadi.Name = "textboxataadi";
+            this.textboxataadi.Size = new System.Drawing.Size(355, 27);
+            this.textboxataadi.TabIndex = 6;
+            // 
+            // textBoXaxtaris
+            // 
+            this.textBoXaxtaris.Location = new System.Drawing.Point(52, 12);
+            this.textBoXaxtaris.Name = "textBoXaxtaris";
+            this.textBoXaxtaris.Size = new System.Drawing.Size(224, 27);
+            this.textBoXaxtaris.TabIndex = 7;
+            // 
+            // buttonaxtar
+            // 
+            this.buttonaxtar.Location = new System.Drawing.Point(313, 12);
+            this.buttonaxtar.Name = "buttonaxtar";
+            this.buttonaxtar.Size = new System.Drawing.Size(166, 33);
+            this.buttonaxtar.TabIndex = 8;
+            this.buttonaxtar.Text = "Axtar";
+            this.buttonaxtar.UseVisualStyleBackColor = true;
+            this.buttonaxtar.Click += new System.EventHandler(this.buttonaxtar_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(12, 310);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 28);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Olke";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(12, 367);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 28);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Seher";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(5, 415);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(74, 28);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Telefon";
+            // 
+            // textboxolke
+            // 
+            this.textboxolke.Location = new System.Drawing.Point(85, 314);
+            this.textboxolke.Name = "textboxolke";
+            this.textboxolke.Size = new System.Drawing.Size(355, 27);
+            this.textboxolke.TabIndex = 12;
+            // 
+            // textboxseher
+            // 
+            this.textboxseher.Location = new System.Drawing.Point(85, 371);
+            this.textboxseher.Name = "textboxseher";
+            this.textboxseher.Size = new System.Drawing.Size(355, 27);
+            this.textboxseher.TabIndex = 13;
+            // 
+            // textboxtelefon
+            // 
+            this.textboxtelefon.Location = new System.Drawing.Point(85, 415);
+            this.textboxtelefon.Name = "textboxtelefon";
+            this.textboxtelefon.Size = new System.Drawing.Size(355, 27);
+            this.textboxtelefon.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(5, 489);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(110, 23);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Dogum Tarixi";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(5, 555);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(77, 23);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Cinsiyyet";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(105, 555);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(53, 24);
+            this.radioButton1.TabIndex = 17;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Kisi";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(206, 555);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(70, 24);
+            this.radioButton2.TabIndex = 18;
+            this.radioButton2.Text = "Qadin";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(160, 489);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(250, 27);
+            this.dateTimePicker1.TabIndex = 19;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(28, 615);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(499, 31);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 658);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textboxtelefon);
+            this.Controls.Add(this.textboxseher);
+            this.Controls.Add(this.textboxolke);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.buttonaxtar);
+            this.Controls.Add(this.textBoXaxtaris);
+            this.Controls.Add(this.textboxataadi);
+            this.Controls.Add(this.textboxad);
+            this.Controls.Add(this.textBoxsoyad);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBoxsoyad;
+        private TextBox textboxad;
+        private TextBox textboxataadi;
+        private TextBox textBoXaxtaris;
+        private Button buttonaxtar;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox textboxolke;
+        private TextBox textboxseher;
+        private TextBox textboxtelefon;
+        private Label label8;
+        private Label label9;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private DateTimePicker dateTimePicker1;
+        private Button button2;
+    }
+}
---
+++ TaskWinformApp/Form1.cs
@@ -0,0 +1,159 @@
+﻿using Newtonsoft.Json;
+using static TaskWinformApp.Form1;
+
+
+namespace TaskWinformApp
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Human
+        {
+            public string ? Name { get; set; }
+            public string ? surname { get; set; }
+            public string ? Ataadi { get; set; }
+            public string ?  olke { get; set; }
+            public string ?  seher { get; set; }
+            public string ?  telefon  { get; set; }
+            public string ? Birtday { get; set; }  
+            
+            public string ? Cinsi { get; set; } 
+
+
+            public Human(string? name, string? surname, string? ataadi, string? olke, string? seher, string? telefon , string? birtday, string? Cinsi)
+            {
+                Name = name;
+                this.surname = surname;
+                Ataadi = ataadi;
+                this.olke = olke;
+                this.seher = seher;
+                this.telefon = telefon;
+                Birtday = birtday;
+                this.Cinsi = Cinsi;
+                
+            }
+
+
+            public Human()
+            {
+
+            }
+
+
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Radio Button yoxlanisi 
+
+        string cinsiyet = radioButton1.Checked ? "Kisi" : "Qadin";
+
+            Human human = new Human
+            {
+                Name = textboxad.Text,
+                surname = textBoxsoyad.Text,
+                Ataadi = textboxataadi.Text,
+                olke = textboxolke.Text,
+                seher = textboxseher.Text,
+                telefon = textboxtelefon.Text,
+                Birtday = dateTimePicker1.Text,
+                Cinsi = cinsiyet
+
+
+
+
+            };
+
+
+
+
+
+            string Jsondata = JsonConvert.SerializeObject(human);
+
+
+            File.WriteAllText("HumanAbout.json",Jsondata );
+        }
+
+
+        private void DeserializeJson(string jsonData)
+        {
+
+
+
+            Human human = JsonConvert.DeserializeObject<Human>(jsonData);
+            string cins = "Kisi";
+            if (human.Cinsi == cins)
+            {
+                radioButton1.Checked= true;
+            }
+            else
+            {
+                radioButton1.Checked = false;
+                radioButton2.Checked= true;
+            }
+            if (human.Name == textBoXaxtaris.Text)
+            {
+
+                textboxad.Text = human.Name;
+                textBoxsoyad.Text = human.surname;
+                textboxataadi.Text = human.Ataadi;
+                textboxolke.Text = human.olke;
+                textboxseher.Text = human.seher;
+                textboxtelefon.Text = human.telefon;
+                dateTimePicker1.Text = human.Birtday;
+                cins = human.Cinsi;
+                
+            }
+            else
+            {
+                textboxad.Text = "";
+                textBoxsoyad.Text = "";
+                textboxataadi.Text = "";
+                textboxolke.Text = "";
+                textboxseher.Text = "";
+                textboxtelefon.Text = "";
+                dateTimePicker1.Text = "";
+                MessageBox.Show("Qeydiyyatdan kecirilmiyib!!");
+
+            }
+
+
+
+        }
+
+        private void buttonaxtar_Click(object sender, EventArgs e)
+        {
+            string fileYeri = "HumanAbout.json";
+
+            try
+            {
+                if (File.Exists(fileYeri))
+                {
+                    string jsonData = File.ReadAllText(fileYeri);
+                    DeserializeJson(jsonData);
+                }
+                else
+                {
+                    MessageBox.Show("Qeyydiyyat edilmir..!.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eror " + ex.Message);
+            }
+
+        }
+
+    }
+}
